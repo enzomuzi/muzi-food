a16ede74e6374a5904f2b1faf1b3572fe365235a
enzomuzi <enzshadows@gmail.com>
2024-09-25 01:10:23 -0300

    Home page corrrigida

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/home-page/css/styles.css">
+    <link rel="stylesheet" href="./home-page/css/styles.css">
     <!--Scripts-->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -46,13 +46,13 @@
                 <div id="carouselExampleFade" class="carousel slide carousel-fade">
                     <div class="carousel-inner">
                         <div class="carousel-item active" data-bs-interval="3000">
-                            <img src="./assets/home-page/img/img-carousel-1.png" class="d-block w-100" alt="Primeiro Slide">
+                            <img src="./home-page/img/img-carousel-1.png" class="d-block w-100" alt="Primeiro Slide">
                         </div>
                         <div class="carousel-item">
-                            <img src="./assets/home-page/img/img-carousel-2.png" class="d-block w-100" alt="Segundo Slide">
+                            <img src="./home-page/img/img-carousel-2.png" class="d-block w-100" alt="Segundo Slide">
                         </div>
                         <div class="carousel-item">
-                            <img src="./assets/home-page/img/img-carousel.png" class="d-block w-100" alt="Terceiro Slide">
+                            <img src="./home-page/img/img-carousel.png" class="d-block w-100" alt="Terceiro Slide">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
