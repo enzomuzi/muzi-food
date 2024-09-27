3a12aa8e33e21be9edaa41ead0dbac320c8cc5d4
Enzo Muzi <enzshadows@gmail.com>
2024-09-27 09:47:54 -0300

    Criação e configuração inicial dos Controllers

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,15 +1,9 @@
 <?php
-// namespace controllers;
 
-// use Controller;
+class HomeController
+{   
+    public function __construct()
+    {
 
-// class HomeController extends Controller {
-
-//     public function __construct() {
-//         parent::__construct(); // Se Controller tem um construtor que inicializa view
-//     }
-
-//     public function index() {
-//         $this->view->render('index'); // Verifique se "index" é o nome correto da view
-//     }
-// }
+    }
+}
