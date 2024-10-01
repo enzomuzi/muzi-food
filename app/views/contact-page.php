6a66ede9630337f904ce0891bfc4f3e7de1ff5ba
Enzo Muzi <enzshadows@gmail.com>
2024-10-01 11:36:16 -0300

    Correção forms página Register

--- app/views/contact-page.php
+++ app/views/contact-page.php
@@ -5,14 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Muzi's Food</title>
     <!-- Fonte -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/home-page/css/styles.css">
+    <link rel="stylesheet" href="/home-page/css/styles.css">
     <!--Scripts-->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -96,7 +96,7 @@
     </div>
     </footer>
     </footer>
-    <script src="/assets/menu-page/js/scripts.js"></script>
+    <script src="/menu-page/js/scripts.js"></script>
 </body>
 
 </html>
--- app/views/register-page.php
+++ app/views/register-page.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Muzi's Food</title>
     <!-- Fonte -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/home-page/css/styles.css">
+    <link rel="stylesheet" href="/home-page/css/styles.css">
     <!--Scripts-->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
@@ -44,49 +44,34 @@
                 <p>Insira suas informações abaixo.</p>
             </div>
             <form class="row g-3">
+            <div class="col-md-6">
+                    <label for="inputFirstName4" class="form-label">Nome</label>
+                    <input type="text" class="form-control" aria-label="First name" required>
+                </div>
+                <div class="col-md-6">
+                    <label for="inputLastName4" class="form-label">Sobrenome</label>
+                    <input type="text" class="form-control" aria-label="Last name" required>
+                </div>
                 <div class="col-md-6">
                     <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4">
+                    <input type="email" class="form-control" id="inputEmail4" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Senha</label>
-                    <input type="password" class="form-control" id="inputPassword4">
+                    <input type="password" class="form-control" id="inputPassword4" required>
                 </div>
                 <div class="col-md-6">
                     <label for="inputPassword4" class="form-label">Confirme sua senha</label>
-                    <input type="password" class="form-control" id="inputPassword4">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputPassword4" class="form-label">Usuário</label>
-                    <input type="password" class="form-control" id="inputPassword4">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress" class="form-label">Endereço</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="Rua, Bairro...">
-                </div>
-                <div class="col-12">
-                    <label for="inputAddress2" class="form-label">Endereço 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartamento, Casa, Loja...">
-                </div>
-                <div class="col-md-6">
-                    <label for="inputCity" class="form-label">Cidade</label>
-                    <input type="text" class="form-control" id="inputCity">
-                </div>
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">Estado</label>
-                    <select id="inputState" class="form-select">
-                        <option selected>Escolha...</option>
-                        <option>...</option>
-                    </select>
+                    <input type="password" class="form-control" id="inputPassword4" required>
                 </div>
                 <div class="col-md-2">
-                    <label for="inputZip" class="form-label">CEP</label>
-                    <input type="text" class="form-control" id="inputZip">
+                    <label for="inputNumber" class="form-label">Telefone</label>
+                    <input type="text" class="form-control" id="inputNumber" required>
                 </div>
                 <div class="col-12">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
+                        <label class="form-check-label" for="gridCheck" required>
                             Aceito os termos e condições
                         </label>
                     </div>
